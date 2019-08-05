f05f85011a0e30313e9349cfec0fc941e4c51359
Steven Galvin <steven.john.galvin@gmail.com>
2019-08-04 21:48:03 -0700

    setup Ruby tutorial

---
+++ hello.rb
@@ -0,0 +1,4 @@
+puts "Hello World!"
+#puts "What is your name?"
+#name = gets.chop
+#puts "Hey, #{name}! Nice to meet you!"
---
+++ montys-color.rb
@@ -0,0 +1,8 @@
+puts "What is your favorite color?"
+color = gets.chop.downcase
+if color == "blue"
+    puts "Wow! My favorite color is #{color} also!"
+else
+    puts "I'm not a big fan of #{color}."
+end
+
