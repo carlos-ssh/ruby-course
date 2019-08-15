25048d3080cca101bfd321d78b7089ed976171bb
Steven Galvin <steven.john.galvin@gmail.com>
2019-08-14 21:06:54 -0700

    break code out into lesson steps and comment out code

--- hello.rb
+++ hello.rb
@@ -1,4 +1,9 @@
-puts "Hello World!"
-#puts "What is your name?"
-#name = gets.chop
-#puts "Hey, #{name}! Nice to meet you!"
+# Lesson 1: Outputting a String
+
+# puts "Hello World!"
+
+# Lesson 2: Prompting User Input
+
+# puts "What is your name?"
+# name = gets
+# puts "Hey, #{name}! Nice to meet you!"
--- montys-color.rb
+++ montys-color.rb
@@ -1,8 +1,23 @@
-puts "What is your favorite color?"
-color = gets.chop.downcase
-if color == "blue"
-    puts "Wow! My favorite color is #{color} also!"
-else
-    puts "I'm not a big fan of #{color}."
-end
+# Lesson 3: Prompt for Favorite Color
 
+# puts "What is your favorite color?"
+# color = gets.chop
+# puts "Wow! My favorite color is #{color} also!"
+
+# Lesson 4: Using an If Statement
+
+# puts "What is your favorite color?"
+# color = gets.chop
+# if color == "blue"
+#     puts "Wow! My favorite color is #{color} also!"
+# end
+
+# Lesson 5: Adding an Else Statement
+
+# puts "What is your favorite color?"
+# color = gets.chop.downcase
+# if color == "blue"
+#     puts "Wow! My favorite color is #{color} also!"
+# else
+#     puts "I'm not a big fan of #{color}."
+# end
