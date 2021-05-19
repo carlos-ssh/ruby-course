f0f54f90337993889fd7b868c0b444f51faa65ad
Carlos  Robles <carlos.anezeh@me.com>
2021-05-19 15:33:05 -0500

    Update file
    Create some lines of code that shows a simple hello world message on console.

--- hello.rb
+++ hello.rb
@@ -1,8 +1,8 @@
 # Lesson 1: Outputting a String
 
-# puts "Hello World!"
+puts "Hello World!"
 
 # Lesson 2: Prompting User Input
 
-# name = gets
-# puts "Hey, #{name}! Nice to meet you!"
+name = gets
+puts "Hey, #{name}! Nice to meet you!"
